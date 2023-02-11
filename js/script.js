9256d908a264e756153a6caac3db26a0ee098f17
Dzmitry Rybak <nakadisa@icloud.com>
2023-02-11 16:54:58 +0100

    Completed all tasks

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 /* 
+
 1) Usuń wszystkie jednostki reklamowe ze strony (prawa strona witryny)
 
 2) Zmień gatunek filmu, Zmień "komedię " na"dramat"
@@ -13,7 +14,11 @@ Posortuj je alfabetycznie
 
 
 'use strict';
-
+const ads = document.querySelectorAll('.promo__adv img'),
+      poster = document.querySelector('.promo__bg'),
+      genre = poster.querySelector('.promo__genre'),
+      movieList = document.querySelector('.promo__interactive-list');
+      
 const movieDB = {
     movies: [
         "Логан",
@@ -24,3 +29,28 @@ const movieDB = {
     ]
 };
 
+// 1) Remote all ads
+ads.forEach(item => {
+    item.remove();
+});
+
+// 2) Change genre
+genre.textContent = "Драма"
+
+// 3) Change backbround img
+poster.style.background = "url('../img/bg.jpg') center center/cover no-repeat";
+
+movieList.innerHTML = '';
+
+// Sorting the list of movies
+movieDB.movies.sort();
+
+// 4, 5) Сreating a list of films from an object movieDB
+
+movieDB.movies.forEach((element, i) => {
+    movieList.innerHTML += `
+    <li class="promo__interactive-item">
+        ${i+1} ${element}
+        <div class="delete"></div>
+    </li>`;
+}) 
