28fa11fc0e7f4d1ff184e23bba28417685232d59
Dzmitry Rybak <nakadisa@icloud.com>
2023-02-11 12:57:33 +0100

    Task movies_proj

---
+++ js/script.js
@@ -0,0 +1,26 @@
+/* 
+1) Usuń wszystkie jednostki reklamowe ze strony (prawa strona witryny)
+
+2) Zmień gatunek filmu, Zmień "komedię " na"dramat"
+
+3) Zmień tło plakatu filmowego na obraz " bg.jpg". Znajduje się w folderze img.
+Zaimplementuj tylko za pomocą JS
+
+4) Lista filmów na stronie Generuj na podstawie danych z tego pliku JS.
+Posortuj je alfabetycznie
+
+5) Dodaj numerację wyświetlanych filmów */
+
+
+'use strict';
+
+const movieDB = {
+    movies: [
+        "Логан",
+        "Лига справедливости",
+        "Ла-ла лэнд",
+        "Одержимость",
+        "Скотт Пилигрим против..."
+    ]
+};
+
